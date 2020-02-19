f12effcdbd58480ed0bc8322cea1ea89e280f5ed
Dr_HuWenqing <huwenqing.pku@gmail.com>
2020-02-19 15:19:44 -0600

    Update reinforcement_learning.py

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Jan  3 14:33:36 2020
-
-@author: huwenqing
-
+@author: Wenqing Hu and Louis Steinmeister
 Title: Reinforcement Learning for the joint control of onsite microgrid and manufacturing system
 """
 output = open('output.txt', 'w') 
@@ -12,8 +10,13 @@ from microgrid_manufacturing_system import Microgrid, ManufacturingSystem, Actio
 from projectionSimplex import projection
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+import math
+#import tensorflow.keras.backend as K
 
-number_machines=4
+
+
+number_machines=5
 
 import pandas as pd
 #read the solar irradiance and wind speed data from file#
@@ -28,12 +31,14 @@ windspeed = np.array(data_wind.iloc[:,3])
 
 
 #the learning rates for the theta and omega iterations#
-lr_theta=0.01
-lr_omega=0.001
+lr_theta=0.003
+lr_omega=0.0003
 
 #the discount factor gamma when calculating the total cost#
-gamma=0.01
+gamma=0.999
 
+#number of iterations#
+number_iteration=10000
 
 """
 Provide the structure of the action-value function Q(S, A^d, A^c, A^r; omega), 
@@ -57,18 +62,12 @@ class action_value(object):
                                                            windspeed=0
                                                            )
                                             ),
-                omega=[ [0 for _ in range(number_machines)], 
-                        [0 for _ in range(number_machines-1)], 
-                        [0 for _ in range(3)],
-                        [0 for _ in range(number_machines)],
-                        [0 for _ in range(3)],
-                        [0 for _ in range(9)],
-                        [0 for _ in range(2)],
-                        0 ] 
+                critic = None
                 ):
+        #define neural network with 2 hidden layers for the Q function
+        self.critic = critic
+        
         self.System=System
-        #omega is the weight vector presented as a numerical list#
-        self.omega=omega       
         
     def num_list_States_Actions(self):
         #return the states and actions as a numerical list#
@@ -81,7 +80,7 @@ class action_value(object):
                 [0 for _ in range(3)],
                 [0 for _ in range(9)],
                 [0 for _ in range(2)],
-                0 ]
+                [0] ]
         for i in range(number_machines):
             if self.System.machine_states[i]=="Off":
                 list[1-1][i]=0
@@ -112,27 +111,105 @@ class action_value(object):
             list[6-1][i+6]=self.System.grid.actions_generator[i]
         for i in range(2):
             list[7-1][i]=self.System.grid.actions_purchased[i]
-        list[8-1]=self.System.grid.actions_discharged
+        list[8-1][0]=self.System.grid.actions_discharged #needs to be a list for later convenience
         return list
         
     
     def Q(self, num_list_States_Actions):
-        #calculate the Q-value#
-        Q=0
-        for i in range(8):
-            Q=Q+np.dot(np.array(self.omega[i]), np.array(num_list_States_Actions[i]))    
-        return Q
+        flat_inputs = np.array([item for sublist in num_list_States_Actions for item in sublist],dtype = "float32")
+        q = self.critic(flat_inputs)[0,0]
+        #print("Evaluation of Critic:", q)
+        return q
     
+    #has to be called after Q
     def Q_grad_A_c(self):
-        #calculate the gradient of Q with respect to A^c#
-        grad=[]
-        for i in range(9):
-            grad.append(self.omega[6-1][i])
-        return np.array(grad)
+        #print("DEBUG DQ_Dinput",self.critic.__Q_grad_input__)
+        #print("DEBUG DQ_DAc",self.critic.__Q_grad_A_c__)
+        return self.critic.__Q_grad_A_c__.numpy()
+
 
     def Q_grad_omega(self, num_list_States_Actions):
-        #calculate the gradient of Q with respect to omega#
-        return num_list_States_Actions
+        #print("Q_grad_omega:", [tensor for tensor in self.critic.__Q_grad_omega__])
+        return [tensor.numpy() for tensor in self.critic.__Q_grad_omega__]
+
+    
+    def update_weights(self, factor):
+        self.critic.update_weights(factor)
+        
+
+
+
+"""
+implements everything related to the Q function
+"""
+class critic():
+    #define the network architecture
+    def __init__(self):
+        self.run_eagerly = True
+        self.dim_input = 3*number_machines-1+18
+        print("inputs expected:",self.dim_input)
+        #self.input = self.layer_input
+        #hidden layers
+        self.layer1     = tf.keras.layers.Dense(100, activation='sigmoid', input_shape = (1,self.dim_input))
+        self.layer2     = tf.keras.layers.Dense(100, activation='relu')
+        #output layer
+        self.layer_out  = tf.keras.layers.Dense(1, activation='linear')
+        #variable to make sure that gradients are computed before applying
+        self.ready_to_apply_gradients = False
+        #store the trainable variables
+        self.trainable_variables = None
+        #execute eager
+        
+
+    #evalueate the Q function for a given (state,action)-pair     
+    def __call__(self, inputs):
+        #flatten inputs for NN
+
+        #the Q value generated by the NN
+        #inputs = self.layer_input(flat_inputs)
+        input_tensor = tf.reshape(inputs,shape = (1,self.dim_input))
+        with tf.GradientTape(persistent=True) as tape:
+            tape.watch(input_tensor)
+            #print("DEBUG:",input_tensor)
+            res     = self.layer1(input_tensor)
+            res     = self.layer2(res)
+            Q_value = self.layer_out(res)
+        #print("",Q_value)
+        if self.trainable_variables == None:
+            self.trainable_variables = self.layer1.variables+self.layer2.variables+self.layer_out.variables
+            #print("DEBUG weights:",self.trainable_variables)
+        #regularization not needed at this time
+        #regularization_loss = tf.math.add_n(self.model.losses)         
+        #compute the gradients of Q wrt omega
+        
+        Q_grad_omega = tape.gradient(Q_value, self.trainable_variables)
+        #print("Q_grad_omega",Q_grad_omega)
+        Q_grad_input = tape.gradient(Q_value, input_tensor)[0]
+        #print("Q_grad_input", Q_grad_input)
+        Q_grad_A_c   = Q_grad_input[-12:-3] #there are 3 entries after A_c and A_c is 9 long
+        self.__Q_grad_omega__ = Q_grad_omega
+        self.__Q_grad_input__ = Q_grad_input
+        self.__Q_grad_A_c__   = Q_grad_A_c
+        self.__Q__            = Q_value
+        self.ready_to_apply_gradients = True
+        
+        return Q_value
+    def update_weights(self, factor):
+        if self.ready_to_apply_gradients == False: raise Exception("not ready to train the critic. Compute gradients first!")
+        #form (gradient, variable)-tuples
+        grad_var_tuples = zip(self.__Q_grad_omega__, self.trainable_variables)
+        #apply update
+        for grad, var in grad_var_tuples:
+            #print("TEST")
+            #print((var-factor*grad).numpy()[0,0])
+            test = (var-factor*grad).numpy()
+            while test.ndim>0:
+                test = test[0]
+            if(math.isnan(test)):
+                raise Exception("NaN Produced! Exiting training.")
+            else:
+                var.assign_add(-factor*grad)
+        self.ready_to_apply_gradients = False
 
 
 
@@ -185,6 +262,7 @@ class update_theta(object):
         
     def deterministic_policygradient(self, A_c_grad_theta, Q_grad_A_c):
         #output the deterministic policy gradient of the cost with respect to the theta#
+        print("Policy gradient; Q_grad_A_c:",Q_grad_A_c)
         policygradient=np.dot(Q_grad_A_c, A_c_grad_theta)
         return policygradient
 
@@ -200,8 +278,16 @@ class update_theta(object):
 """
 Reinforcement Learning Algorithm: Off policy TD control combined with actor-critique
 Algorithm 1 in the paper
+when optimal policy is found, must add
+1. Total cost and throughput in given time horizon that the 
+algorithm is used to guide the bilateral control.
+2. Total energy demand across all time periods of the given 
+time horizon and the proportion of the energy supply to satisfy the demand.
+ 
 """
 if __name__ == "__main__":
+    tf.enable_eager_execution()
+    #K.clear_session()
     grid=Microgrid(workingstatus=[0,0,0],
                    SOC=0,
                    actions_adjustingstatus=[0,0,0],
@@ -215,29 +301,32 @@ if __name__ == "__main__":
                    )
     System=ManufacturingSystem(machine_states=["Off" for _ in range(number_machines)],
                                machine_control_actions=["K" for _ in range(number_machines)],
-                               buffer_states=[0 for _ in range(number_machines-1)],
+                               buffer_states=[2 for _ in range(number_machines-1)],
                                grid=grid
                                )
     
     #randomly generate an initial theta and plot the bounday of the simplex where theta moves#
     r=np.random.uniform(0,1,size=6)
+    
+    #initialize the theta variable#
     theta=[r[0]*r[1], r[0]*(1-r[1]), r[2]*r[3], r[2]*(1-r[3]), r[4]*r[5], r[4]*(1-r[5])] 
+    thetainit=theta
+    
     x = [[0, 0], [0, 1], [1, 0]] 
     y = [[0, 1], [1, 0], [0, 0]]
+    plt.figure(figsize = (14,10))
     for i in range(len(x)): 
         plt.plot(x[i], y[i], color='g')
     
-    omega=[ [0 for _ in range(number_machines)], 
-            [0 for _ in range(number_machines-1)], 
-            [0 for _ in range(3)],
-            [0 for _ in range(number_machines)],
-            [0 for _ in range(3)],
-            [0 for _ in range(9)],
-            [0 for _ in range(2)],
-            0 ] 
+    my_critic = critic()
+    omega = []
     Q=[]
-
-    for t in range(1000):
+    TemporalDifference=[]
+    rewardseq=[]
+    reward=0
+    diff_omega=[]
+       
+    for t in range(number_iteration):
         #current states and actions S_t and A_t are stored in class System#
         print("*********************Time Step", t, "*********************", file=output)
         for i in range(number_machines):
@@ -266,7 +355,7 @@ if __name__ == "__main__":
         print(" ", file=output)
         print("Average Total Cost=", E, file=output)
         #calculate the old Q-value and its gradient wrt omega: Q(S_t, A_t; omega_t) and grad_omega Q(S_t, A_t; omega_t)#
-        av=action_value(System, omega)
+        av=action_value(System,my_critic)
         num_list_SA=av.num_list_States_Actions()
         print("states and actions in numerical list=", np.array(num_list_SA), file=output)
         Q_old=av.Q(num_list_SA)
@@ -326,16 +415,32 @@ if __name__ == "__main__":
                                    )        
         
         #TD-control SARSA#
-        av=action_value(System, omega)
+        av=action_value(System, my_critic)
         num_list_SA=av.num_list_States_Actions()
         Q_new=av.Q(num_list_SA)
         TD=E+gamma*Q_new-Q_old
         print("TD=", TD, file=output)
+        TemporalDifference.append(TD)
+        #calculate the up-to-date reward#
+        reward=reward+np.power(gamma, t)*E
+        rewardseq.append(reward)
+        print("cumulative reward=", reward, file=output)
         #update omega using actor-critique#
         print("Q_grad_omega=", Q_grad_omega_old, file=output)
         factor=lr_omega*TD
         print("lr_omega*TD=", factor, file=output)
         print("omega=", np.array(omega), file=output)
+        my_critic.update_weights(factor)
+        #update and calculate the norm difference in omega#
+        if t==0:
+            diff_omega.append(0)
+            omega = [var.numpy() for var in my_critic.trainable_variables]
+        else:
+            omega_old=omega
+            omega = [var.numpy() for var in my_critic.trainable_variables]
+            omega_new=omega
+            diff_omega.append(np.sum([np.linalg.norm(new_var-old_var) for new_var, old_var in zip(omega_new,omega_old)]))
+        """
         for i in range(number_machines):
             omega[1-1][i]=omega[1-1][i]-factor*Q_grad_omega_old[1-1][i]
         for i in range(number_machines-1):
@@ -352,11 +457,11 @@ if __name__ == "__main__":
             omega[6-1][i+6]=omega[6-1][i+6]-factor*Q_grad_omega_old[6-1][i+6]
         for i in range(2):
             omega[7-1][i]=omega[7-1][i]-factor*Q_grad_omega_old[7-1][i]
-        omega[8-1]=omega[8-1]-factor*Q_grad_omega_old[8-1]
+        omega[8-1]=omega[8-1]-factor*Q_grad_omega_old[8-1]"""
         
         #discount the learning rate#
         lr_theta=lr_theta*1
-        lr_omega=lr_omega*0.99
+        lr_omega=lr_omega*0.999
         
         print(" ", file=output)
         
@@ -364,11 +469,55 @@ if __name__ == "__main__":
         plt.scatter(theta[0], theta[1], color='b')
         plt.scatter(theta[2], theta[3], marker='+', color='m')
         plt.scatter(theta[4], theta[5], marker='*', color='r')
-
+        #end of the iteration loop for reinforcement learning training process#
+    
+    #plot the theta dynamics#
+    plt.savefig('theta.png')
+    plt.show()  
+       
+    #plot the Q values#
+    plt.figure(figsize = (14,10))
+    plt.plot(Q)
+    plt.xlabel('iteration')
+    plt.ylabel('action-value-function')
+    plt.savefig('Q.png')
+    plt.show() 
+    
+    #plot the temporal differences#
+    plt.figure(figsize = (14,10))
+    plt.plot(TemporalDifference)
+    plt.xlabel('iteration')
+    plt.ylabel('temporal difference function')
+    plt.savefig('TD.png')
     plt.show()   
     
-    #plot the Q values#
-    plt.plot(Q)
+    #plot the reward sequences#
+    plt.figure(figsize = (14,10))
+    plt.plot(rewardseq)
+    plt.xlabel('iteration')
+    plt.ylabel('Sum of rewards during episode')
+    plt.savefig('rewards.png')
+    plt.show()       
+
+    #plot the weight difference sequences#
+    plt.figure(figsize = (14,10))
+    plt.plot(diff_omega)
+    plt.xlabel('iteration')
+    plt.ylabel('L2 norm of the difference in the weights')
+    plt.savefig('weightdifference.png')
     plt.show()   
+    
+    #with the optimal theta and optimal omega at hand, run the system at a certain time horizon.
+    #Calculate 1. Total cost and throughput in given time horizon that the algorithm is used to guide the bilateral control.
+    #Calculate 2. Total energy demand across all time periods of the given time horizon. 
+
+    #output the optimal theta and optimal omega#
+    thetaoptimal=theta
+    omegaoptimal=omega   
+    print("initial proportion of energy supply=", thetainit)
+    print("optimal proportion of energy supply=", thetaoptimal)
+    print("optimal paramter for the neural-network integrated action-value function=", omegaoptimal)
+    
+    
     
 output.close() 
